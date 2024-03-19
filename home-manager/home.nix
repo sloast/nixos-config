6025b1bc52b51fadde23a6f309dfc51367fec27b
sloast <adaadrobson@gmail.com>
2024-03-19 20:32:21 +0000

    nixos-rebuild

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -69,6 +69,31 @@
     };
   };
 
+  gtk = {
+    enable = true;
+    font.name = "TeX Gyre Adventor 10";
+    theme = {
+      name = "Juno";
+      package = pkgs.juno-theme;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git.enable = true;
