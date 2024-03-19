5505580c7f46fe2b1e8b5f61cc45f9528b97db00
sloast <adaadrobson@gmail.com>
2024-03-19 20:33:15 +0000

    Generation 174   2024-03-19 16:24:31

--- modules/hyprland/bin/swww-init.sh
+++ modules/hyprland/bin/swww-init.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-logfile="/home/adaad/swwwlog.log"
+date
 
-date &>> $logfile
+rm /run/user/1000/swww.socket
+rm -r "$HOME/.cache/swww"
 
-rm /run/user/1000/swww.socket &>> $logfile
-rm -r "$HOME/.cache/swww" &>> $logfile
-
-swww query &>> $logfile
+swww query
 #if [ $? -eq 1 ] ; then
-#    swww init &>> $logfile
+#    swww init
 #fi
 
-swww init &>> $logfile
-echo "$?" &>> "$logfile"
-#swww img "$HOME/wallpapers/wall_anime2_4K.png" --transition-type any &>> $logfile
-swww-random.sh &>> "$logfile"
+swww init
+#swww img "$HOME/wallpapers/wall_anime2_4K.png" --transition-type any
+swww-random.sh
 
