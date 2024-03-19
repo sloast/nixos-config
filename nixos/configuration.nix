e181a9465a0c7532a12b6e720b76ddcb433e0c41
sloast <adaadrobson@gmail.com>
2024-03-19 20:32:21 +0000

    Generation   34   2024-03-16 15:37:44

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,6 +9,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./hyprland.nix
     ./home-manager-import.nix
   ];
 
@@ -80,9 +81,6 @@
     gh
     gnumake
     grim
-    hyprland
-    swaylock
-    swww
     jetbrains.idea-community
     kitty
     libsForQt5.kdenlive
@@ -98,42 +96,13 @@
     slurp
     unzip
     vscode
-    waybar
     wireplumber
     wget
-    #wlroots
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-hyprland
-    xdg-utils
-    #xwayland
   ];
 
   fonts.fonts = with pkgs; [
     nerdfonts
   ];
-
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  programs.waybar.enable = true;
-
-  # swaylock
-  # https://discourse.nixos.org/t/swaylock-wont-unlock/27275
-  security.pam.services.swaylock = {};
-  security.pam.services.swaylock.fprintAuth = false;
-
-  services.greetd = {
-    enable = true;
-    settings = rec {
-      initial_session = {
-        command = "Hyprland && swaylock";
-        user = "adaad";
-      };
-      default_session = initial_session;
-    };
-  };
 
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
---
+++ nixos/hyprland.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    hyprland
+    swaylock
+    swww
+    waybar
+    #wlroots
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+    xdg-utils
+    #xwayland
+  ];
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  programs.waybar.enable = true;
+
+  # swaylock
+  # https://discourse.nixos.org/t/swaylock-wont-unlock/27275
+  security.pam.services.swaylock = {};
+  security.pam.services.swaylock.fprintAuth = false;
+
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "Hyprland && swaylock";
+        user = "adaad";
+      };
+      default_session = initial_session;
+    };
+  };
+}
