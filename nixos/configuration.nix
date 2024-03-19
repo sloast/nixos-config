e11e3cd287c70a934a23d3f42f75573dea2298b6
sloast <adaadrobson@gmail.com>
2024-03-19 20:32:21 +0000

    nixos-rebuild

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,6 +11,8 @@
     ./hardware-configuration.nix
   ];
 
+  nix.settings.experimental-features = "nix-command flakes";
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
