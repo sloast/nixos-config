74a1a6a0c51071cf693a4997fea23ae9a16abaef
sloast <adaadrobson@gmail.com>
2024-04-16 15:27:50 +0100

    Generation 528   2024-04-16 15:27:49

--- modules/waybar/home.nix
+++ modules/waybar/home.nix
@@ -96,7 +96,7 @@ in {
 
       "network" = {
         # "interface": "wlp2*"; // (Optional) To force the use of this interface
-        format-wifi = "{essid} ({signalStrength}%) ";
+        format-wifi = "{essid}   {signalStrength}%";
         format-ethernet = "{ipaddr}/{cidr} ";
         tooltip-format = "{ifname} via {gwaddr} ";
         format-linked = "{ifname} (No IP) ";
