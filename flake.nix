87ba123a1359ffaa7c34cd960f3304610f4b0795
sloast <adaadrobson@gmail.com>
2024-09-30 11:04:08 +0100

    Bump nixpkgs to 24.5

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # unstable
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
